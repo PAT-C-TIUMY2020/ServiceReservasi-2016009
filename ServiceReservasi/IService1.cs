5faf0020d6200cffce90d7fd61f2b0b298e1ed8b
Arrasy <arrasyjunior@gmail.com>
2020-11-23 12:28:39 +0700

    Add project files.

---
+++ ServiceReservasi/IService1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceReservasi
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string pemesanan(string IDPemesanan, string NamaCustomer, string NoTelepon, int JumlahPemesanan, string IDLokasi);
+        [OperationContract]
+        string editPemesanan(string IDPemesanan, string NamaCustomer);
+        [OperationContract]
+        string deletePemesanan(string IDPemesanan);
+        [OperationContract]
+        List<CekLokasi> ReviewLokasi();
+        [OperationContract]
+        List<DetailLokasi> DetailLokasi();
+        [OperationContract]
+        List<Pemesanan> Pemesanan();
+        string GetData(int value);
+
+    }
+
+    [DataContract]
+    public class CekLokasi //daftar lokasi nongkrong
+    {
+        [DataMember]
+        public string IDLokasi { get; set; } //variabel dari public class
+        [DataMember]
+        public string NamaLokasi { get; set; }
+        [DataMember]
+        public string DeskripsiSingkat { get; set; }
+    }
+
+    [DataContract]
+    public class DetailLokasi //menampilkan detail lokasi
+    {
+        [DataMember]
+        public string IDLokasi { get; set; } //variabel dari public class
+        [DataMember]
+        public string NamaLokasi { get; set; }
+        [DataMember]
+        public string DeskripsiFull { get; set; }
+        [DataMember]
+        public int Kuota { get; set; }
+    }
+
+    [DataContract]
+    public class Pemesanan //daftar lokasi nongkrong
+    {
+        [DataMember]
+        public string IDPemesanan { get; set; } 
+        [DataMember]
+        public string NamaCustomer { get; set; } //method
+        [DataMember]
+        public string NoTelepon { get; set; }
+        [DataMember]
+        public int JumlahPemesanan { get; set; }
+        [DataMember]
+        public string IDLokasi { get; set; }
+    }
+
+
+   //DataContract]
+    //public class CompositeType
+    //{
+      //  bool boolValue = true;
+      // string stringValue = "Hello ";
+
+        //[DataMember]
+        //public bool BoolValue
+        //{
+            //get { return boolValue; }
+          //  set { boolValue = value; }
+       // }
+
+        //[DataMember]
+        //public string StringValue
+        //{
+          //  get { return stringValue; }
+          //  set { stringValue = value; }
+        //}
+    //}
+}
